e67311cda92a5ac608b83b1d78b4839b0389b36f
Fredy Mata <fredymata17@icloud.com>
2024-03-19 02:57:57 -0500

    Added game resources and initial settings file.
    Added timeframe clock.
    Moved initial settings to settings.py.
    Improved comments.

--- galactic_guardian.py
+++ galactic_guardian.py
@@ -1,29 +1,43 @@
 import sys
 import pygame
+from settings import Settings
 
 
 class GalacticGuardian:
-    """Main class to manage game resources and conduct."""
+    """Main class to manage game resources and conduct behavior."""
     def __init__(self):
-        """Initialize the pygame module (game) and create resources (screen)."""
+        """
+        Initializes the pygame module (game).
+        Creates resources (timeframe clock, screen).
+        Initializes the game settings for this module.
+        """
         pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Galactic Guardian")
 
     def run_game(self):
-        """Start the main loop for the game to run continuously."""
+        """Starts the main loop for the game to run continuously."""
         while True:
-            # watch for keyboard and mouse events
+            # watches for keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            # make the most recently drawn screen visible
+            # redraws the screen each pass through the loop
+            self.screen.fill(self.settings.bg_color)
+
+            # makes the most recently drawn screen visible
             pygame.display.flip()
+
+            # defines the frame rate so that the clock can make the loop run this many times per second
+            self.clock.tick(60)
 
 
 if __name__ == "__main__":
-    # make a game instance and run the game
+    # makes a game instance and runs the game
+    # this makes it so the instance can only be run from this module, meaning no multiple game instances
     ai = GalacticGuardian()
     ai.run_game()
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class to define all settings for Galactic Guardian."""
+    def __init__(self):
+        """Initializes the game's settings for other modules."""
+        # screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (54, 1, 63)
