d28cd6a886fe1cbb8becf448b5bff6856ab7c19a
Fredy Mata <fredymata17@icloud.com>
2024-03-29 22:26:01 -0500

    Created new statistics module.
    Created class GameStats to track statistics of different aspects of the game.

---
+++ game_stats.py
@@ -0,0 +1,13 @@
+from settings import Settings
+
+
+class GameStats:
+    """Track statistics for Galactic Guardian."""
+    def __init__(self, gg_game):
+        """Initialize statistics."""
+        self.settings = gg_game.settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.settings.ship_limit
