ad467828dde4ad3883c01733363a6f0c12b52e7f
Fredy Mata <fredymata17@icloud.com>
2024-04-08 03:06:09 -0500

    Changed the drop speed of the aliens to make game more challenging.

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ class Settings:
         self.bullets_allowed = 3
 
         # alien settings
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 25
 
         # how quickly the game speeds up
         self.speedup_scale = 1.1
