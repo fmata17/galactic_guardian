40555d05745cd6c37f2703f939eb436143819a5a
Fredy Mata <fredymata17@icloud.com>
2024-04-09 18:42:56 -0500

    Added code in the scoreboard module.
    -Added new prep_spaceships method to create a group of spaceship sprites so they could represent the lives left in each game.
    -Updated other methods so they could respond to the new group and show it on screen when the show_score method is called.

--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,14 @@
 import pygame.font
+from pygame.sprite import Group
+
+from spaceship import Spaceship
 
 
 class Scoreboard:
     """A class to report the score."""
     def __init__(self, gg_game):
         """Initialize the score keeping attribute"""
+        self.gg_game = gg_game
         self.screen = gg_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = gg_game.settings
@@ -18,6 +22,7 @@ class Scoreboard:
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
+        self.prep_spaceships()
 
     def prep_score(self):
         """Turn the score into a rendered image"""
@@ -59,8 +64,18 @@ class Scoreboard:
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
 
+    def prep_spaceships(self):
+        """Show how many spaceships are left."""
+        self.spaceships = Group()
+        for spaceship_number in range(self.stats.spaceships_left):
+            spaceship = Spaceship(self.gg_game)
+            spaceship.rect.x = 20 + spaceship_number * spaceship.rect.width
+            spaceship.rect.y = 20
+            self.spaceships.add(spaceship)
+
     def show_score(self):
         """Show the score on the screen."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        self.spaceships.draw(self.screen)
